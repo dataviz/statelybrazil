cd9f0bd54683fb8a523a0a6d1f3450b8ad841a5b
Leo Tartari <ltartari@gmail.com>
2013-02-07 01:54:39 -0200

    adds brazil font, html and config.rb for compass configuration

---
+++ brazil/config.rb
@@ -0,0 +1,11 @@
+# Require any additional compass plugins here.
+project_type = :stand_alone
+
+# Publishing paths
+http_path = "/"
+http_images_path = "/img"
+http_fonts_path = "/fonts"
+css_dir = "/css"
+
+line_comments = true
+output_style = :extended

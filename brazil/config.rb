eda492a245cab29ee1e5f06115aef81bcb58c78d
Leo Tartari <ltartari@gmail.com>
2013-02-07 18:43:35 -0200

    Compass configuration

--- brazil/config.rb
+++ brazil/config.rb
@@ -1,11 +1,26 @@
 # Require any additional compass plugins here.
 project_type = :stand_alone
 
-# Publishing paths
 http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/sass"
+images_dir = "images"
 http_images_path = "/img"
+javascripts_dir = "javascripts"
 http_fonts_path = "/fonts"
-css_dir = "/css"
 
-line_comments = true
 output_style = :extended
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# line_comments = false
+line_comments = true
+
+
+# If you prefer the indented syntax, you might want to regenerate this
+# project again passing --syntax sass, or you can uncomment this:
+# preferred_syntax = :sass
+# and then run:
+# sass-convert -R --from scss --to sass assets/sass scss && rm -rf sass && mv scss sass
